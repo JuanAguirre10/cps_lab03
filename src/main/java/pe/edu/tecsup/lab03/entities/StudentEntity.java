8438bcd6aea9b4b59d043fd001cb1eb00e8da05e
Luis Enrique Galvan Morales <136293012+LuisGalvan1512@users.noreply.github.com>
2025-09-04 14:57:11 -0500

    Update StudentEntity.java
    xdxdxd

--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -1,36 +1,4 @@
 package pe.edu.tecsup.lab03.entities;
 
 public class StudentEntity {
-    private Long id;
-    private String name;
-    private String email;
-
-    // Constructor vacío
-    public StudentEntity() {
-    }
-
-    // Getters y setters básicos
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
