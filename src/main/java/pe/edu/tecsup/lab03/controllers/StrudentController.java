223696a12a699b241d4e96843f85ac6a084a6237
Luis Galvan <lusen121502@gmail.com>
2025-09-04 15:16:36 -0500

    Colaborador: modificaciones en StudentController, StudentService, StudentRepository, StudentEntity

--- src/main/java/pe/edu/tecsup/lab03/controllers/StrudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StrudentController.java
@@ -1,4 +1,10 @@
 package pe.edu.tecsup.lab03.controllers;
-
+/**
+ * StudentController
+ * ------------------
+ * Controlador que gestiona las solicitudes relacionadas con estudiantes.
+ *
+ * Modificado por Luis Enrique Galvan Morales - Sprint 2
+ */
 public class StrudentController {
 }
--- src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
+++ src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
@@ -1,4 +1,12 @@
 package pe.edu.tecsup.lab03.services;
 
+/**
+ * StudentService
+ * ----------------
+ * Lógica de negocio para gestionar los estudiantes.
+ *
+ * Modificado por Luis Enrique Galvan Morales - Sprint 2
+ */
+
 public class StudentService {
 }
